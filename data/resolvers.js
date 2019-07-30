6f94dfb3e0a095ce745a9aafeb7747aa35c13eb2
johnnyhicks95 <johnnysomerhald@gmail.com>
2019-07-29 20:21:49 -0500

    Corregido la coneccion a Mongo, agregado mutations para eliminar registros, agregado querys de consultar todos los clientes con algun limite  y cliente por id

--- data/resolvers.js
+++ data/resolvers.js
@@ -6,9 +6,23 @@ import { rejects } from 'assert'
 export const resolvers = {
     //con la configuracion de graphql-tools para mapear elementos
     Query: {
-        getCliente: ({ id }) => {
-            return new Cliente(id, clientesDB[id])
-        }
+
+        //para enlistar todos los clientes
+        getClientes: ( root, { limite }) => {
+            return Clientes.find( {} ).limit(limite)
+        },
+
+        //trae un cliente segun el modelo de mongoose
+        getCliente: ( root, { id }) => {
+            return new Promise ( (resolve, object) => {
+                //llamamos por el id y pasamos el error, pero si
+                //la coneccion es exitosa trae los datos del cliente
+                Clientes.findById(id, (error, cliente) => {
+                    if(error) rejects(error)
+                    else resolve(cliente)
+                })
+            } )
+        },
     },
     Mutation: {
         crearCliente: (root, { input }) => {
@@ -17,7 +31,7 @@ export const resolvers = {
                 nombre: input.nombre,
                 apellido: input.apellido,
                 empresa: input.empresa,
-                emails: input.email,
+                emails: input.emails,
                 edad: input.edad,
                 tipo: input.tipo,
                 pedidos: input.pedidos
@@ -39,8 +53,10 @@ export const resolvers = {
                 // con un metodo de mongoose buscamos el _id de mongo
                 // filtro, valor ,opciones  -new true crea un archivo nuevo si no tiene ese id
                 Clientes.findOneAndUpdate(
+                    // primero selecciona el filtro en este caso el id de la bd en mongo
                     {_id: input.id},
                     input,
+                    // true si el registro no existe crea uno nuevo
                     { new: true },
                     (error, cliente) => {
                         if (error) rejects(error)
@@ -48,6 +64,18 @@ export const resolvers = {
                     }
                 )
             })
+        },
+
+        //Mutation para eliminar cliente por id segun schema.graphql
+        eliminarCliente: (root, { id }) => {
+            return new Promise( (resolve, object ) => {
+                Clientes.findOneAndRemove( {_id: id }, (error ) =>{
+                    if (error) rejects (error)
+                    else resolve("El dato del cliente se eliminó correctamente")
+                } )
+            })
         }
+
+
     }
 }
